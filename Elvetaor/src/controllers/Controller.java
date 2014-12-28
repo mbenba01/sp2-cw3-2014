032c11b9f609ff461153af5b8555049ce6996ff2
= <=>
2014-12-28 20:50:09 +0000

    implementing user input error checking in Controller

--- Elvetaor/src/controllers/Controller.java
+++ Elvetaor/src/controllers/Controller.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import classes.Building;
@@ -14,22 +15,37 @@ public class Controller {
 	
 	public static void main(String[] args) {
 		
-		/*//Initialise new object 'in'
-		in = new Scanner(System.in);
-		int numberOfFloors;
-		//prompt user for a number of floors to use as a parameter for the Building object
-		System.out.print("Please enter the number of floors: ");
-		numberOfFloors = in.nextInt();
-		 
-		//prompt user for and store number of customers to use as a parameter for the Building object
-		System.out.print("Please enter the number of customers: ");
-		int numberOfCustomers = in.nextInt();
+		//Initialise new object 'in'
+		//in = new Scanner(System.in);
+		int numberOfFloors = 15;
+		int numberOfCustomers = 10;
+		boolean userInput=true;
+		
+		do{
+			try{
+				in = new Scanner(System.in);
+				//prompt user for a number of floors to use as a parameter for the Building object
+				System.out.print("Please enter the number of floors: ");
+				numberOfFloors = in.nextInt();
+				 
+				//prompt user for and store number of customers to use as a parameter for the Building object
+				System.out.print("Please enter the number of customers: ");
+				numberOfCustomers = in.nextInt();
+				userInput = false;
+			}catch (InputMismatchException e){
+				System.out.println("wrong input, please try again!!!");
+				userInput=true;
+				continue;
+			}
+			
+			userInput=false; // in order to finish while loop
+		}while(userInput); 
 		
 		//Close Scanner object 'in' after use.
-		in.close();*/
+		in.close();
 		
 		//Create new instance of Building
-		Building theHyde = new Building(15, 10);
+		Building theHyde = new Building(numberOfFloors, numberOfCustomers);
 		//Elevator lift = new Elevator(15);
 		ArrayList customersInBuilding = theHyde.getCustomerList();
 		ArrayList<Customer> customersInBuilding2 = new ArrayList<Customer>(customersInBuilding);
